21283ef36fba12ae7abd55f22d10125331a895a3
damyks <damianojeh@gmail.com>
2025-03-07 00:07:42 +0100

    added null to  og image field

--- opportunity/models.py
+++ opportunity/models.py
@@ -21,7 +21,7 @@ class Opportunity(models.Model):
     application_links = models.ManyToManyField(
         ApplicationLink, related_name="opportunities", blank=True
     )
-    og_image_url = models.URLField(blank=True)
+    og_image_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.title} - {self.category}"
